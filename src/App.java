8e5c4009d3ec9db4a36d03089bd6e8e68ea24acb
Ursoo <ursu.calin18@gmail.com>
2024-08-13 22:21:04 +0300

    added writing to file still wip

--- src/App.java
+++ src/App.java
@@ -1,13 +1,15 @@
-import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        RandomPasswordGenerator pass_gen = new RandomPasswordGenerator();
-        RandomPasswordGenerator pass_gen2 = new RandomPasswordGenerator(24);
+        RandomPasswordGenerator pass_gen = new RandomPasswordGenerator(24);
 
-        System.out.println(pass_gen.getPasswordLen());
-        System.out.println(pass_gen.getGeneratedPass());
-        System.out.println(pass_gen2.getPasswordLen());
-        System.out.print(pass_gen2.getGeneratedPass());
+        PasswordVaultManager vault_manager = new PasswordVaultManager();
+
+        String value;
+
+        for(int i = 0; i< 10; i++){
+            vault_manager.updateVault(""+i);
+    }
+
     }
 }
