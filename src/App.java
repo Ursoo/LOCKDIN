bef2b211853e6aa3ed1d33c4c2339f387829dd04
Ursoo <ursu.calin18@gmail.com>
2024-08-21 14:53:39 +0300

    added new class to control the way an user interacts with the app

--- src/App.java
+++ src/App.java
@@ -1,15 +1,7 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        RandomPasswordGenerator pass_gen = new RandomPasswordGenerator(24);
-
-        PasswordVaultManager vault_manager = new PasswordVaultManager();
-
-        String value;
-
-        for(int i = 0; i< 10; i++){
-            vault_manager.updateVault(""+i);
-    }
+        ControlFlowManager flow_manager = new ControlFlowManager();
 
     }
 }
