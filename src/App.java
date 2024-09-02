ac0c54b06c9c3c5a435aea6432b99ba80a8632e3
Ursoo <ursu.calin18@gmail.com>
2024-09-02 22:48:08 +0300

    restructured classes

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ControlFlowManager flow_manager = new ControlFlowManager();
+            ControlFlowManager.run();
 
     }
 }
--- src/ControlFlowManager.java
+++ src/ControlFlowManager.java
@@ -1,64 +1,37 @@
 import java.util.Scanner;
 import java.awt.datatransfer.StringSelection;
+import java.io.IOException;
 import java.awt.Toolkit;
 
 public class ControlFlowManager {
-    private int choice;
-    private Scanner input_reader =  new Scanner(System.in);
-    private RandomPasswordGenerator pass_gen =  new RandomPasswordGenerator();
-    private PasswordVaultManager vault_manager = new PasswordVaultManager();
+    private static int choice;
+    private static Scanner input_reader =  new Scanner(System.in);
+    private static PasswordVaultManager vault_manager = new PasswordVaultManager();
 
-    public ControlFlowManager(){
-        while(this.run());
-    }
 
-    private boolean run(){
+    public static void run(){
 
+        while(true){
         System.out.println("Select an option:");
         System.out.println("1. Create a new password");
         System.out.println("2. Create a password vault");
-        System.out.println("3. Exit");
-        System.out.println("Your choice (must be one of the numbers): ");
-        this.choice = input_reader.nextInt();
+        System.out.println("3. List all identifiers");
+        System.out.println("4. Exit");
+        System.out.print("Your choice (must be one of the numbers): ");
+        choice = input_reader.nextInt();
 
-        if(this.choice == 1){
-            this.createPassword();
-        } else if(this.choice == 2){
-            //
+        switch (choice) {
+            case 1:
+                addPasswordToVault();
+                break;
+            default:
+                break;
         }
-        else if(this.choice == 3){
-            input_reader.close();
-            return false;
-        }
-
-        return true;
     }
 
-    private void createPassword(){
-        String password = pass_gen.generate_password();
-
-        System.out.println(password);
-
-        Toolkit.getDefaultToolkit()
-        .getSystemClipboard()
-        .setContents(new StringSelection(password), null);
-
-        System.out.println("Password copied to clipboard!");
-        System.out.print("Do you want to save to password vault (y/n)?: ");
-
-        String user_input = input_reader.nextLine();
-
-        switch(user_input.toLowerCase()){
-            case "y":
-            case "yes": {
-                System.out.println("Selection:" + user_input);
-                break;}
-            case "n":
-            case "no":{
-                System.out.println("Selection:" + user_input);
-                break;}
-        }
-
-
     }
+
+    private static void addPasswordToVault(){
+    }
+
 }
